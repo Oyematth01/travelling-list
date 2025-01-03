d05583e70602574cbaa16ef894cac522359130fb
Oyematth01 <oyematth@gmail.com>
2025-01-03 18:08:44 +0100

    Updating component to correctly display items

--- src/js/PackingList.js
+++ src/js/PackingList.js
@@ -1,49 +1,21 @@
-import { useState } from "react";
-import Item from "./Item";
-
-export default function PackingList({
-  items,
-  onDeleteItem,
-  onToggleItem,
-  onClearList,
-}) {
-  const [sortBy, setSortBy] = useState("input");
-
-  let sortedItems;
-
-  if (sortBy === "input") sortedItems = items;
-
-  if (sortBy === "description")
-    sortedItems = items
-      .slice()
-      .sort((a, b) => a.description.localeCompare(b.description));
-
-  if (sortBy === "packed")
-    sortedItems = items
-      .slice()
-      .sort((a, b) => Number(a.packed) - Number(b.packed));
-
+export default function PackingList({ items, onDeleteItem, onToggleItem }) {
   return (
     <div className="list">
+      <h2>Packing List</h2>
       <ul>
-        {sortedItems.map((item) => (
-          <Item
-            item={item}
-            onDeleteItem={onDeleteItem}
-            onToggleItem={onToggleItem}
-            key={item.id}
-          />
+        {items.map((item) => (
+          <li key={item.id}>
+            <input
+              type="checkbox"
+              checked={item.packed}
+              onChange={() => onToggleItem(item.id)}
+            />
+            <span>{item.description}</span>
+            <span>({item.quantity})</span>
+            <button onClick={() => onDeleteItem(item.id)}>Delete</button>
+          </li>
         ))}
       </ul>
-
-      <div className="actions">
-        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
-          <option value="input">Sort by input order</option>
-          <option value="description">Sort by description</option>
-          <option value="packed">Sort by packed status</option>
-        </select>
-        <button onClick={onClearList}>Clear list</button>
-      </div>
     </div>
   );
 }
