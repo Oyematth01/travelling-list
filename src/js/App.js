424cb10415f2dedc4101f5ed415d1cb5bd4d50c3
Oyematth01 <oyematth@gmail.com>
2025-01-06 22:30:11 +0100

    Updating the app.js to configure to changes made from other components

--- src/js/App.js
+++ src/js/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Route, Routes, useNavigate, Link } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Logo from "./Logo";
 import Form from "./Form";
 import PackingList from "./PackingList";
